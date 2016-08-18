ca05474ec27f29d716239b932f64b976ad9e65dc
Brian S. Corbin <corbinbs@gmail.com>
2016-08-17 22:29:16 -0400

    Projector refinements after testing with a ViewSonic PJD5155

--- pivideo/projector.py
+++ pivideo/projector.py
@@ -5,11 +5,14 @@ import time
 
 DEFAULT_PORT = '/dev/ttyUSB0'
 
+CMD_ACK = '\x03\x14\x00\x00\x00\x14'
+
 ERROR_STATUS_CMD = '\x07\x14\x00\x05\x00\x34\x00\x00\x0C\x0D\x66'
 
 POWER_ON_CMD     = '\x06\x14\x00\x04\x00\x34\x11\x00\x00\x5D'
 POWER_OFF_CMD    = '\x06\x14\x00\x04\x00\x34\x11\x01\x00\x5E'
 POWER_STATUS_CMD = '\x07\x14\x00\x05\x00\x34\x00\x00\x11\x00\x5E'
+POWER_IS_ON_RESPONSE = '\x05\x14\x00\x03\x00\x00\x00\x01\x18'
 
 INPUT_SOURCE_VGA_CMD       = '\x06\x14\x00\x04\x00\x34\x13\x01\x00\x60'
 INPUT_SOURCE_VGA2_CMD      = '\x06\x14\x00\x04\x00\x34\x13\x01\x08\x68'
@@ -27,6 +30,9 @@ LAMP_MODE_DYNAMIC_CMD  = '\x06\x14\x00\x04\x00\x34\x11\x10\x02\x6F'
 LAMP_MODE_SLEEP_CMD    = '\x06\x14\x00\x04\x00\x34\x11\x10\x03\x70'
 LAMP_MODE_STATUS_CMD   = '\x07\x14\x00\x05\x00\x34\x00\x00\x11\x10\x6E'
 
+PROJECTOR_POSITION_FRONT_TABLE_CMD = '\x06\x14\x00\x04\x00\x34\x12\x00\x00\x5E'
+PROJECTOR_POSITION_REAR_TABLE_CMD = '\x06\x14\x00\x04\x00\x34\x12\x00\x01\x5F'
+
 
 class Projector(object):
 
@@ -34,9 +40,6 @@ class Projector(object):
         self.port_name = port_name
         self.baud_rate = baud_rate
         self.serial_port = None
-        # Time to wait (in seconds) before attempting to receive data after a
-        # command has been issued
-        self.receive_wait_time = 0.5
 
     def __enter__(self):
         self.connect()
@@ -46,56 +49,76 @@ class Projector(object):
         self.disconnect()
 
     def connect(self):
-        self.serial_port = serial.Serial(self.port_name, self.baud_rate)
+        self.serial_port = serial.Serial(port=self.port_name,
+                                         baudrate=self.baud_rate,
+                                         bytesize=serial.EIGHTBITS,
+                                         parity=serial.PARITY_NONE,
+                                         stopbits=serial.STOPBITS_ONE,
+                                         writeTimeout = 0,
+                                         timeout=5,
+                                         xonxoff=False,
+                                         rtscts=False,
+                                         dsrdtr=False)
 
     def disconnect(self):
         if self.serial_port:
-            self.serial_port.close
+            self.serial_port.close()
             self.serial_port = None
 
     def send(self, command):
         if self.serial_port:
             self.serial_port.write(command)
 
-    def receive(self):
+    def receive(self, byte_count=1):
         """
             Receive data from connected projector
             :return: the data returned by the projector
         """
         if self.serial_port:
-            #Wait for data to become available
-            time.sleep(self.receive_wait_time)
-            retry_number = 0
-            value = ""
-            while True:
-                data = self.serial_port.read(1)
+            return self.serial_port.read(byte_count)
 
-                if data == '>':
-                    break
+        return ''
 
-                if len(data) == 0:
-                    if retry_number >= 10:
-                        break
-                    retry_number += 1
-                    continue
+    def flush_input(self):
+        if self.serial_port:
+            self.serial_port.flushInput()
 
-                if data == '\r':
-                    continue
+    def input_source_hdmi(self):
+        self.flush_input()
+        self.send(INPUT_SOURCE_HDMI_CMD)
+        return self.receive(byte_count=6) == CMD_ACK
 
-                value += data
-
-            if value:
-                return value
-
-        return None
-
+    def input_source_vga(self):
+        self.flush_input()
+        self.send(INPUT_SOURCE_VGA_CMD)
+        return self.receive(byte_count=6) == CMD_ACK
 
     def power_on(self):
+        self.flush_input()
         self.send(POWER_ON_CMD)
+        return self.receive(byte_count=6) == CMD_ACK
 
     def power_off(self):
+        self.flush_input()
         self.send(POWER_OFF_CMD)
+        return self.receive(byte_count=6) == CMD_ACK
 
     def is_on(self):
+        self.flush_input()
         self.send(POWER_STATUS_CMD)
-        return self.receive()
+        return self.receive(byte_count=9) == POWER_IS_ON_RESPONSE
+
+    def front_table_position(self):
+        self.flush_input()
+        self.send(PROJECTOR_POSITION_FRONT_TABLE_CMD)
+        return self.receive(byte_count=6) == CMD_ACK
+
+    def rear_table_position(self):
+        self.flush_input()
+        self.send(PROJECTOR_POSITION_REAR_TABLE_CMD)
+        return self.receive(byte_count=6) == CMD_ACK
+
+    def reset_settings(self):
+        self.flush_input()
+        self.send(RESET_ALL_SETTINGS_CMD)
+        return self.receive(byte_count=6) == CMD_ACK
